e4f3fe507a5fa8895462055bb8c78f00fb9582c9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-03-22 18:54:09 +0900

    add: main.dartを作成

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:comic_check_app/views/todo_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: () => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodoListPage(),
+      ),
+    );
+  }
+}
