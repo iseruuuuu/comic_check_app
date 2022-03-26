b4118676d0123377d91b87ebe491920c063df000
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-03-26 13:52:39 +0900

    add: dataのmodelを追加

---
+++ lib/models/todo.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Todo _$_$_TodoFromJson(Map<String, dynamic> json) {
+  return _$_Todo(
+    id: json['id'] as int?,
+    title: json['title'] as String? ?? '',
+    publisher: json['publisher'] as String? ?? '',
+    count: json['count'] as int? ?? 0,
+  );
+}
+
+Map<String, dynamic> _$_$_TodoToJson(_$_Todo instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'publisher': instance.publisher,
+      'count': instance.count,
+    };
