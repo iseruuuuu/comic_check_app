294617c47800d20fd81f4c87a42c3ac1c7b758a5
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-03-26 13:51:26 +0900

    add: main.dartを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:comic_check_app/views/todo_list_page.dart';
+import 'package:comic_check_app/views/todo_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,11 +21,8 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: () => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: TodoListPage(),
+        debugShowCheckedModeBanner: false,
+        home: TodoListScreen(),
       ),
     );
   }
