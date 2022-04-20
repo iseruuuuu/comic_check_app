09f6099c55ceaceb10816a5231ca78d4347cfd60
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-04-20 11:19:06 +0900

    add: add const

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: () => MaterialApp(
+      builder: () => const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: TodoListScreen(),
       ),
