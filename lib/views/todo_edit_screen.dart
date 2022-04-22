15274b9bf63a4352cdd31c94e6e9e79fdfc4ec12
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-04-22 10:39:44 +0900

    add: edit_screenで更新できるようにした

--- lib/views/todo_edit_screen.dart
+++ lib/views/todo_edit_screen.dart
@@ -1,12 +1,15 @@
-import 'package:comic_check_app/models/publisher.dart';
 import 'package:comic_check_app/models/todo.dart';
 import 'package:comic_check_app/viewModels/todo_view_model.dart';
+import 'package:comic_check_app/views/children/edit_button.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'children/back_icon.dart';
+import 'children/picker_item.dart';
+import 'children/text_field_item.dart';
+import 'children/title_text.dart';
 
 class TodoEditScreen extends HookWidget {
   const TodoEditScreen({
@@ -19,148 +22,72 @@ class TodoEditScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final todoViewModel = useProvider(todoViewModelProvider.notifier);
-    final titleController = TextEditingController(text: todo.title);
     final comicState = useProvider(todoViewModelProvider);
-    final comic = comicState.publisher;
-    final count = comicState.count;
-    useEffect(() {
-      WidgetsBinding.instance?.addPostFrameCallback(
-        (_) {},
-      );
-    });
+    useEffect(
+      () {
+        WidgetsBinding.instance?.addPostFrameCallback(
+          (_) {
+            todoViewModel.initState(
+              comicName: todo.title,
+              publisher: todo.publisher,
+              count: todo.count,
+            );
+          },
+        );
+      },
+      const [],
+    );
     return Scaffold(
+      backgroundColor: CupertinoColors.secondarySystemBackground,
       appBar: AppBar(
-        backgroundColor: Colors.redAccent,
+        backgroundColor: CupertinoColors.secondarySystemBackground,
         elevation: 0,
+        foregroundColor: Colors.black,
+        leading: const BackIcon(
+          icon: Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            '漫画名',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 30.w),
+            const TitleText(title: 'コミック名'),
+            TextFieldItem(
+              onChanged: (word) => todoViewModel.onChangeWord(word),
+              comicName: todo.title,
+              hintText: '漫画名',
+              maxLength: 20,
+            ),
+            SizedBox(height: 20.w),
+            const TitleText(title: '出版社'),
+            PickerItem(
+              title: comicState.publisher,
               fontSize: 20.w,
+              onTap: () async => await todoViewModel.openPublisher(context),
             ),
-          ),
-          SizedBox(
-            width: 300.w,
-            child: TextField(
-              //controller: titleController,
-              style: TextStyle(fontSize: 20.w),
-              autocorrect: false,
-              onChanged: (comicName) {
-                todoViewModel.onChangeWord(comicName);
-              },
+            SizedBox(height: 30.w),
+            const TitleText(title: '漫画の巻数'),
+            PickerItem(
+              title: '${comicState.count}' '巻',
+              fontSize: 30.w,
+              onTap: () async => await todoViewModel.openComicCount(context),
             ),
-          ),
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: '出版社',
-                  style: TextStyle(
-                    fontSize: 22.w,
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                TextSpan(
-                  text: ' / ',
-                  style: TextStyle(
-                    fontSize: 25.w,
-                    color: Colors.black,
-                  ),
-                ),
-                TextSpan(
-                  //TODO 初期画面は、事前に登録したもので、変更したら変わるようにする。
-                  text: todoViewModel.publisher(),
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22.w,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.width / 2,
-            child: CupertinoPicker(
-              backgroundColor: Colors.grey.shade200,
-              looping: true,
-              itemExtent: 30,
-              scrollController: FixedExtentScrollController(initialItem: 0),
-              onSelectedItemChanged: (index) {
-                todoViewModel.changePublisher(publisher[index]);
-              },
-              children: publisher.map((publisher) => Text(publisher)).toList(),
-            ),
-          ),
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: '巻数',
-                  style: TextStyle(
-                    fontSize: 35.w,
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                TextSpan(
-                  text: ' / ',
-                  style: TextStyle(
-                    fontSize: 35.w,
-                    color: Colors.black,
-                  ),
-                ),
-                TextSpan(
-                  //TODO 初期画面は、事前に登録したもので、変更したら変わるようにする。
-                  text: todoViewModel.text(count: todo.count) + '巻',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 35.w,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () async {
-                      await todoViewModel.openComicCount(context);
-                    },
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width / 1.2,
-            height: 40.w,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.redAccent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
+            const Spacer(),
+            EditButton(
               onPressed: () async {
-                //TODO　更新の処理をしっかりとかく
-                if (titleController.text != '' && comic != '' && count != 0) {
+                if (comicState.comicName != '' &&
+                    comicState.publisher != '' &&
+                    comicState.count != 0) {
                   await todoViewModel.updateComic(todo.id!, todo);
                   Navigator.pop(context);
                   Navigator.pop(context);
                 }
               },
-              child: Text(
-                '更新',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.w,
-                  color: Colors.white,
-                ),
-              ),
             ),
-          ),
-        ],
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
